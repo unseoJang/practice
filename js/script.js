28613a94372cc22a453a87d01eb959031a04112c
DESKTOP-RBS8IG3\user <qqbck123@naver.com>
2018-06-12 16:07:54 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
-var slide;
+var $slide;
 var slideIdx = 0;
 var slideLength = 0;
 var autoslide = '';
 
 $(function(){
 	var slide = $('.slide_wrap');
-	// slideLength = $slide.find('.slide_view ul li').length;
+	slideLength = $slide.find('.slide_view ul li').length;
 	// autoslide = setInterval(slideNext, 4000);
-	slide.find('.bt_control a').click(function(){
+	$slide.find('.bt_control a').click(function(){
 
 		if($(this).attr('class') == 'bt_prev') prevSlide();
 		else if($(this).attr('class') == 'bt_next') nextSlide();
 		return false;
 	});
-
 });
 
 function nextSlide(){//bt_next의 함수
 	// console.log('a');
 	if(slideIdx == slideLength-1) slideIdx=0;
 	else slideIdx++;//slideIdx = slideIdx+1;
+	slideMotion();
 }
 function prevSlide(){//bt_prev의 함수
 	// console.log('b');
 	if(slideIdx == 0) slideIdx = slideLength-1;
 	else slideIdx--;//slideIdx = slideIdx-1;
+	slideMotion();
 }
 function slideMotion(){
-	slide.find('.slide_view ul li').animate({"margin-left":-slide.find('.slide_view ul li').outerWidth()*currIdx});
+	$slide.find('.slide_view ul li').animate({"margin-left":-$slide.find('.slide_view ul li').outerWidth()*currIdx});
 
 	caruselMotion();
 }
